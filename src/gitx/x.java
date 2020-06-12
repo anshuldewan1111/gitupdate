8ff1ad7e9149a7c292ffe5020e26854286182ce4
Anshul Dewan <anshuldewan1111@gmail.com>
2020-06-12 16:57:08 +0530

    this is update commit from asian time

---
+++ src/gitx/x.java
@@ -0,0 +1,18 @@
+package gitx;
+
+public class x {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("postjira");
+	
+		System.out.println("postjira2");
+		
+		System.out.println("postjira3");
+		
+		
+		
+	}
+
+}
